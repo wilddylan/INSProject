c0d835b701a0335e093558a58a13a3cfb60466ea
Dylan <3664132@163.com>
2016-10-21 18:47:18 +0800

    Convert from vscode to webStorm

--- index.ios.js
+++ index.ios.js
@@ -1,20 +1,1 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
-import { Router, Scene } from 'react-native-router-flux';
-import GuidePage from './GuidePage';
-import RootPage from './RootPage';
-
-class Project extends Component {
-  render() {
-    return (
-      <Router>
-        <Scene key="root">
-          <Scene  key="GuidePage" component={GuidePage} hideNavBar title="GuidePage" initial={this.props.launched} />
-          <Scene key="RootPage" component={RootPage} title="RootPage" hideNavBar initial={this.props.launched}/>
-        </Scene>
-      </Router>
-    );
-  };
-};
-
-AppRegistry.registerComponent('Project', () => Project);
+import React, { Component } from 'react';import { AppRegistry } from 'react-native';import { Router, Scene } from 'react-native-router-flux';import GuidePage from './GuidePage';import RootPage from './RootPage';class Project extends Component {  render() {    return (      <Router>        <Scene key="root">          <Scene  key="GuidePage" component={GuidePage} hideNavBar title="GuidePage" initial={this.props.launched} />          <Scene key="RootPage" component={RootPage} title="RootPage" hideNavBar initial={this.props.launched}/>        </Scene>      </Router>    );  };};AppRegistry.registerComponent('Project', () => Project);
